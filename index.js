5c6412b35c4381f6e8e10be8213ae0552630aba0
gpenverne <gpenverne@gmail.com>
2018-02-24 10:32:08 +0100

    reorder

--- index.js
+++ index.js
@@ -8,19 +8,35 @@ var client = amazon.createClient(conf.aws);
 
 var server = http.createServer(function(req, res) {
   const searchTerms = decodeURI(req.url).replace('/', '');
-  return client.itemSearch({
-       keywords: searchTerms,
-       responseGroup: 'ItemAttributes,Offers,Images',
-       domain: conf.aws.domain
-    }).then(function(results){
+  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8', 'Access-Control-Allow-Origin': '*'});
+  respond(res, searchTerms);
+});
+
+function respond(res, searchTerms) {
+    try {
+        return client.itemSearch({
+             keywords: searchTerms,
+             responseGroup: 'ItemAttributes,Offers,Images',
+             domain: conf.aws.domain
+         }).then(function(results){
+             handleProducts(res, results);
+         }).catch(function(err){
+             console.log(JSON.stringify(err));
+         });
+    } catch (e) {
+        respond(res);
+    }
+}
+function handleProducts(res, results) {
+    console.log("handleProducts");
+    try {
         shuffle(results);
-        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
         const product = results[0];
         const friendlyProduct = {
-          asin: product.ASIN[0],
-          title: product.ItemAttributes[0].Title[0],
-          url: product.DetailPageURL[0],
-          pictures: {
+            asin: product.ASIN[0],
+            title: product.ItemAttributes[0].Title[0],
+            url: product.DetailPageURL[0],
+            pictures: {
               small: {
                   url: product.SmallImage[0].URL[0],
                   height: parseInt(product.SmallImage[0].Height[0]['_']),
@@ -36,19 +52,20 @@ var server = http.createServer(function(req, res) {
                   height: parseInt(product.LargeImage[0].Height[0]['_']),
                   width: parseInt(product.LargeImage[0].Width[0]['_'])
               }
-          },
-          features: product.ItemAttributes.Feature,
-          binding: product.ItemAttributes.Binding,
-          price: {
+            },
+            features: product.ItemAttributes.Feature,
+            binding: product.ItemAttributes.Binding,
+            price: {
               currency: product.ItemAttributes[0].ListPrice[0].CurrencyCode[0],
               amountInCents:  parseInt(product.ItemAttributes[0].ListPrice[0].Amount),
               amount:  parseInt(product.ItemAttributes[0].ListPrice[0].Amount) / 100
-          }
+            }
         };
-        res.end(JSON.stringify(friendlyProduct));
-    });
-});
-
+        return res.end(JSON.stringify(friendlyProduct));
+    } catch(e) {
+        return handleProducts(res, results);
+    }
+}
 server.listen(conf.port);
 
 console.log("Listen on "+conf.port);
